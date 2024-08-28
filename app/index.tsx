c75874a651776be7b059cb2d2d225f3867e449ce
Felipe <felipekislyy@gmal.com>
2024-08-27 22:08:13 -0300

    configurado

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { Text, 
+  View, 
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Pressable,
+  Linking,
+  TextInput, } from "react-native";
+
+export default function Index() {
+  
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <TextInput>
+
+      </TextInput>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
