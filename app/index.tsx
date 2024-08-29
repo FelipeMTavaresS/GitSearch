bf323efe9cba9602cd643ba3f3eb1b1d2a394137
Felipe <felipekislyy@gmal.com>
2024-08-29 15:52:52 -0300

    função formatNumber

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { View, Text, Image } from "react-native";
 import { 
-  View, 
-  Text
-   } from "react-native";
-import {
   TextInput,
   ProfileIconContainer,
-  ProfileImage
-} from './styled';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+  ProfileImage, 
+  InputContainer, 
+  SearchButton,
+  BoxContainer,
+  TextTitle, 
+  TextSubTitle,
+  ViewSpace,
+  ProfileText
+} from "./styled";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const PlaceHolderImagem = "https://img.icons8.com/pulsar-color/192/test-account.png";
+const LupaIcon = "https://img.icons8.com/ios-filled/50/000000/search--v1.png";
+
+
+
+function formatNumber(value: number): string {
+  if (value >= 1000 && value < 1000000) {
+    return (value / 1000).toFixed(1).replace('.0', '') + 'k'; // Ex: 1500 -> 1.5k
+  } else if (value >= 1000000) {
+    return (value / 1000000).toFixed(1).replace('.0', '') + 'M'; // Ex: 1500000 -> 1.5M
+  } else {
+    return value.toString(); // Ex: 500 -> 500
+  }
+}
 
 export default function Index() {
-  const [userName, setUserName] = useState<String>('')
-  const [login, setLogin] = useState('');
-  const [name, setName] = useState('');
-  const [avatarUrl, setAvatarUrl] = useState('');
-  const [bio, setBio] = useState('');
+  const [userName, setUserName] = useState<String>("");
+  const [login, setLogin] = useState("");
+  const [name, setName] = useState("");
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const [bio, setBio] = useState("");
   const [publicRepos, setPublicRepos] = useState(0);
   const [followers, setFollowers] = useState(0);
   const [following, setFollowing] = useState(0);
+  const [location, setLocation] = useState("");
+  const [id, setId] = useState("");
 
-  
   const handleGetUserData = async () => {
     if (!userName) {
-      alert('Por favor, insira um nome de usuário.');
+      alert("Por favor, insira um nome de usuário.");
       return;
     }
 
     try {
       const response = await fetch(`https://api.github.com/users/${userName}`);
       if (!response.ok) {
-        throw new Error('Usuário não encontrado');
+        throw new Error("Usuário não encontrado");
       }
       const data = await response.json();
 
@@ -41,38 +62,51 @@ export default function Index() {
       setPublicRepos(data.public_repos);
       setFollowers(data.followers);
       setFollowing(data.following);
+      setLocation(data.location || "Localização não disponível");
+      setId(data.id);
+      
     } catch (error) {
-      console.error('Erro ao buscar os dados do usuário:', error);
-      alert('Erro ao buscar os dados do usuário.');
+      console.error("Erro ao buscar os dados do usuário:", error);
+      alert("Erro ao buscar os dados do usuário.");
     }
   };
 
-  
-
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-      }}
-    >
-      <TextInput
-        onChangeText={(text) => setUserName(text)}
-        placeholder="Digite seu nome"
-      />
-      <button onClick={handleGetUserData}></button>
-      
-      <Text></Text>
-      <TouchableOpacity>
-      <ProfileIconContainer>
-        <ProfileImage source={{ uri: avatarUrl }} />
-      </ProfileIconContainer>
-      </TouchableOpacity>
-      <Text>Nome: {name}</Text>
-      <Text>User: {userName}</Text>
-      <Text>Seguidores: {followers}</Text>
-
-    </View>
-    
+    <SafeAreaView>
+      <View style={{ flex: 1, alignItems: "center" }}>
+        <InputContainer>
+          <TextInput
+            onChangeText={(text) => setUserName(text)}
+            placeholder="Nome do usuário"
+          />
+          <SearchButton onPress={handleGetUserData}>
+            <Image source={{ uri: LupaIcon }} style={{ width: 20, height: 20 }} />
+          </SearchButton>
+        </InputContainer>
+        
+        <TouchableOpacity>
+          <ProfileIconContainer>
+            <ProfileImage source={{ uri: avatarUrl || PlaceHolderImagem }} />
+          </ProfileIconContainer>
+        </TouchableOpacity>
+        <BoxContainer>
+          <ProfileText>
+          <Text>{name || "Usuário"}</Text>
+          <Text>{login || "User"}</Text>
+          <Text>País: {location || ""}</Text>
+          <Text>{id}</Text>
+          </ProfileText>
+          <ViewSpace>
+            <TextTitle>{formatNumber(followers)}</TextTitle>
+            <TextTitle>{publicRepos}</TextTitle>
+          </ViewSpace>
+          <ViewSpace>
+            <TextSubTitle>Seguidores</TextSubTitle>
+            <TextSubTitle>Repos</TextSubTitle>
+          </ViewSpace>
+          
+        </BoxContainer>
+      </View>
+    </SafeAreaView>
   );
 }
--- app/styled.tsx
+++ app/styled.tsx
@@ -1,25 +1,52 @@
+import styled from 'styled-components/native';
 
-import styled from 'styled-components/native'
+export const InputContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  height: 45px;
+  width: 80%;
+  border-radius: 22px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1); 
+  elevation: 1; 
+  margin-top: 20px;
+  margin-bottom: 40px;
+  padding-left: 15px;
+`;
 
-
+export const BoxContainer = styled.View`
+  position: absolute;
+  margin-top: 150;
+  width: 320px;
+  height: 280px;
+  background-color: #fff;
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+  justify-content: center;
+  align-items: center;
+  z-index: -1;
+`;
 
 
 export const TextInput = styled.TextInput`
-  background-color: gray;
-  color: black;
-  padding: 10px 20%;
-  border-radius: 15px;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  position: relative;
-  z-index: 1000;
+  flex: 1;
+  font-size: 16px;
+  color: #808080;
 `;
+
+export const SearchButton = styled.TouchableOpacity`
+  padding: 10px;
+  margin-right: 10px; /* Espaço à direita para o botão */
+  justify-content: center;
+  align-items: center;
+`;
+
 export const ProfileIconContainer = styled.View`
   width: 100px;
   height: 100px;
   border-radius: 50px;
   overflow: hidden;
-  border: 2px solid #4CAF50;
   justify-content: center;
   align-items: center;
 `;
@@ -29,3 +56,32 @@ export const ProfileImage = styled.Image`
   height: 100%;
   object-fit: cover;
 `;
+
+export const TextTitle = styled.Text`
+  color: #333;
+  font-size: 48px; 
+  font-weight: bold; 
+  text-align: center;
+  
+`;
+
+export const TextSubTitle = styled.Text`
+  color: #333;
+  font-size: 12px;
+  text-align: center;
+  
+`;
+
+
+export const ViewSpace = styled.View`
+  flex-direction: row; /* Coloca os subtítulos em linha */
+  justify-content: space-around; /* Espaça os subtítulos uniformemente */
+  align-items: center;
+  width: 100%;
+  margin-top: 10px; /* Espaçamento entre títulos e subtítulos */
+`;
+
+export const ProfileText = styled.View`
+  margin-top: 20px;
+  align-items: center;
+`
